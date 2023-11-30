41c61768fa55ead3362011d7f79fb97f54e1dab7
Ben <btmolloy2@gmail.com>
2023-11-30 05:19:25 -0500

    Update test_StartUIMenu.py

--- tests/test_StartUIMenu.py
+++ tests/test_StartUIMenu.py
@@ -1,31 +1,83 @@
 import pytest
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 from src.TetrisStartMenu import TetrisStartMenu
+import asyncio
+import pygame
+"""
+@pytest.mark.asyncio
+async def test_menu_ui_appearance():
+    # Create an instance of TetrisStartMenu
+    start_menu = TetrisStartMenu()
 
-# Mocking pygame for testing
-pygame_mock = patch("src.TetrisStartMenu.pygame", autospec=True).start()
+    # Create a task to run the initialize method
+    task = asyncio.create_task(start_menu.initialize())
+    await asyncio.sleep(1)
 
-# Mocking PygameDelegate and PygameDelegate.init()
-pygame_mock.PygameDelegate.return_value.init.return_value = None
+    assert start_menu.startGameFlag == False
 
-# Mocking the start button clickCheck method
-@pytest.fixture
-def start_menu():
-    with patch("src.TetrisUIButton.UIButton") as mock_ui_button:
-        mock_ui_button.return_value.clickCheck.return_value = True
-        return TetrisStartMenu()
+    # Stop the UI
+    start_menu.stop_ui()
 
-def test_start_menu_ui(start_menu):
-    assert start_menu.running is True
+    # Cancel the task
+    task.cancel()
 
-    with patch("src.TetrisStartMenu.pygame.display.flip") as mock_flip:
-        start_menu.initialize()
+    # Wait for the task to be cancelled
+    with pytest.raises(asyncio.CancelledError):
+        await task
 
-    mock_flip.assert_called_once()
+    # Delete the start_menu object
+    start_menu = None
+"""
 
-    assert start_menu.start_button.draw.called
-    assert start_menu.quit_button.draw.called
-    assert start_menu.start_button.clickCheck.called
-    assert start_menu.quit_button.clickCheck.called
+@pytest.mark.asyncio
+async def test_menu_start_button():
+    # Create an instance of TetrisStartMenu
+    start_menu = TetrisStartMenu()
 
-    # Add more assertions based on your specific requirements
+    # Mock the behavior of the start button
+    with patch.object(start_menu.start_button, 'clickCheck', MagicMock(return_value=True)):
+        # Simulate clicking the start button by calling buttonHandle with a mock event
+        async def simulate_start_button():
+            await asyncio.sleep(0)  # Ensure the event loop runs
+
+            # Mock event data for a mouse button down event at position (x, y)
+            x, y = 200, 305
+            mock_event = MagicMock()
+            mock_event.type = pygame.MOUSEBUTTONDOWN
+            mock_event.pos = (x, y)
+
+            # Call buttonHandle with the mock event
+            start_menu.buttonHandle([mock_event])
+
+        # Run the simulate_start_button coroutine
+        await simulate_start_button()
+
+@pytest.mark.asyncio
+async def test_menu_quit_button():
+    # Create an instance of TetrisStartMenu
+    start_menu = TetrisStartMenu()
+
+    # Mock the behavior of the quit button to raise SystemExit
+    with patch.object(start_menu.quit_button, 'clickCheck', MagicMock(return_value=True)):
+        # Set running to False to break out of the infinite loop
+        start_menu.running = False
+
+        # Simulate clicking the quit button by calling buttonHandle with a mock event
+        async def simulate_quit_button():
+            await asyncio.sleep(0)  # Ensure the event loop runs
+
+            # Mock event data for a mouse button down event at position (x, y)
+            x, y = 200, 370
+            mock_event = MagicMock()
+            mock_event.type = pygame.MOUSEBUTTONDOWN
+            mock_event.pos = (x, y)
+
+            # Call buttonHandle with the mock event
+            with pytest.raises(SystemExit) as exc_info:
+                start_menu.buttonHandle([mock_event])
+
+            # Check if the raised exception is SystemExit
+            assert exc_info.type == SystemExit
+
+        # Run the simulate_quit_button coroutine
+        await simulate_quit_button()
